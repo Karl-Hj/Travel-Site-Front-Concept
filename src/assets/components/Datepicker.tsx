54e45637ef2efe9262d651dbc377f00ac251c84e
b19karhj <sc2rena@gmail.com>
2024-11-07 00:19:08 +0100

    Solution 2 for datepicker.
    The second solution for datepicker after reading up on more documentation. Keeping this solution as it solves the leap years and is less and easier to read code.

--- src/assets/components/Datepicker.tsx
+++ src/assets/components/Datepicker.tsx
@@ -1,63 +1,27 @@
-import { useEffect, useReducer } from "react";
 import "./css/datePicker.css";
-interface Action {
-  type: number;
-}
-
-function reducer(state: number, action: Action) {
-  switch (action.type) {
-    case 0:
-      return 31;
-    case 1:
-      return 28;
-    case 2:
-      return 31;
-    case 3:
-      return 30;
-    case 4:
-      return 31;
-    case 5:
-      return 30;
-    case 6:
-      return 31;
-    case 7:
-      return 31;
-    case 8:
-      return 30;
-    case 9:
-      return 31;
-    case 10:
-      return 30;
-    case 11:
-      return 31;
-    default:
-      return state;
-  }
-}
 
 export function DatePicker() {
   const currentMonth = new Date().getMonth();
-  // Get the day of the first date of the month
-  const year = new Date().getFullYear();
-  const firstDayOfMonth = new Date(year, currentMonth, 1).getDay();
-  console.log(currentMonth);
+  const currentYear = new Date().getFullYear();
+  /*Sending in year to check for leap year.
+  Fetching the next month (+1) and getting the last date of the month before(currentMonth)*/
+  const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+  //Gets the current day of the week
+  const firstDayIndex = new Date(currentYear, currentMonth, 1).getDay();
+  console.log("Month :" + currentMonth);
+  console.log("Year :" + currentYear);
+  console.log("daysInMonth :" + daysInMonth);
+  console.log("currentDay :" + firstDayIndex);
 
-  const [amountOfDays, dispatch] = useReducer(reducer, currentMonth);
-
-  //   Loads array with current Month.
-  useEffect(() => {
-    dispatch({ type: currentMonth });
-  }, [currentMonth]);
-
-  const days: number[] = Array(firstDayOfMonth)
+  const arrayOfDays: (number | null)[] = Array(firstDayIndex)
     .fill(null)
-    .concat(Array.from({ length: amountOfDays }, (_, index) => index + 1));
-
+    .concat(Array.from({ length: daysInMonth }, (_, index) => index + 1));
+  console.log("Array :" + arrayOfDays);
   return (
     <div className="date-picker-container">
       <div className="date-picker-days-container">
         <div className="date-grid-container">
-          {days.map((day, index) => {
+          {arrayOfDays.map((day, index) => {
             return (
               <div className="days" key={index}>
                 {day !== null ? day : ""}
